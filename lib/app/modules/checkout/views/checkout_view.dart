61cc9a46385a21ddc1967f31ddd53c28c29288c9
Airin Ristiana <airinristianaaa@gmail.com>
2025-07-05 18:35:00 +0700

    updated checkout page

--- lib/app/modules/checkout/views/checkout_view.dart
+++ lib/app/modules/checkout/views/checkout_view.dart
@@ -1,4 +1,6 @@
+import 'package:chingu_app/shared/constant/colors.dart';
 import 'package:chingu_app/shared/constant/text_styles.dart';
+import 'package:chingu_app/shared/widgets/custom_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
@@ -18,7 +20,7 @@ class CheckoutView extends GetView<CheckoutController> {
       ),
       body: SafeArea(
         child: Padding(
-          padding: EdgeInsets.symmetric(horizontal: 16.w),
+          padding: EdgeInsets.only(top: 16.h, left: 16.w, right: 16.w),
           child: SingleChildScrollView(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
@@ -55,6 +57,65 @@ class CheckoutView extends GetView<CheckoutController> {
                 ),
                 Padding(padding: EdgeInsetsGeometry.only(bottom: 20.h)),
                 Text("Transaction Details", style: AppTextStyles.label),
+                SizedBox(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      transactionDetails(text: "Seats", value: "F5, F6"),
+                      transactionDetails(
+                        text: "2 Tickets",
+                        value: "Rp. 40.000 x 2",
+                      ),
+                      transactionDetails(
+                        text: "Service Fee",
+                        value: "Rp. 5.000 x 2",
+                      ),
+                      Text(
+                        "For support our future services",
+                        style: AppTextStyles.smallText,
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 20.h)),
+                      transactionDetails(text: "Total", value: "Rp. 90.000"),
+                    ],
+                  ),
+                ),
+                Padding(padding: EdgeInsetsGeometry.only(bottom: 30.h)),
+                Padding(
+                  padding: EdgeInsets.only(bottom: 10.h),
+                  child: Text("Payment Method", style: AppTextStyles.label),
+                ),
+                SizedBox(
+                  height: 160.h,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      CustomButton(
+                        text: "QRIS",
+                        onPressed: () {},
+                        backgroundColor: Colors.black,
+                      ),
+                      CustomButton(
+                        text: "E-WALLET",
+                        onPressed: () {},
+                        backgroundColor: Colors.black,
+                      ),
+                      CustomButton(
+                        text: "Debit",
+                        onPressed: () {},
+                        backgroundColor: Colors.black,
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsetsGeometry.only(top: 30.h),
+                  child: CustomButton(
+                    text: "Confim",
+                    onPressed: () {},
+                    backgroundColor: AppColors.primary,
+                  ),
+                ),
               ],
             ),
           ),
@@ -62,4 +123,17 @@ class CheckoutView extends GetView<CheckoutController> {
       ),
     );
   }
+}
+
+Widget transactionDetails({required String text, required String value}) {
+  return Padding(
+    padding: EdgeInsets.only(top: 12.h),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(text, style: AppTextStyles.body),
+        Text(value, style: AppTextStyles.body,),
+      ],
+    ),
+  );
 }
