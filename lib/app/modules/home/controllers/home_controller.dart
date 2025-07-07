fd7e76615cda6ad8b62f328e9e89bf62ad3d4502
Mohamad Zaki Zakiran <zzakiran4@gmail.com>
2025-07-07 11:49:23 +0700

    updated variables

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -4,8 +4,14 @@ import 'package:get/get.dart';
 class HomeController extends GetxController {
   final count = 0.obs;
   final searchController = TextEditingController();
-  final movieTitle = "Jumbo"; 
-  final movieSynopsis = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices turpis"; 
+  final moviePoster = "assets/images/jumbo-poster.png".obs;
+  final movieTitle = "Jumbo";
+  final movieSynopsis =
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac ultrices turpis";
+  final movieYear = "2024";
+  final movieGenre = "Drama";
+  final movieDuration = "120 min";
+  final movieRating = 4.5;
   void increment() => count.value++;
 
   var isObscure = true.obs;
