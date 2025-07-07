5421e8b3c1e0737f434c132faf6c04e94f95f097
Mohamad Zaki Zakiran <zzakiran4@gmail.com>
2025-07-07 11:04:28 +0700

    fixed home UI

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,4 +1,5 @@
 import 'package:chingu_app/shared/constant/text_styles.dart';
+import 'package:chingu_app/shared/widgets/custom_card.dart';
 import 'package:chingu_app/shared/widgets/custom_text_field.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,71 +12,62 @@ class HomeView extends GetView<HomeController> {
   const HomeView({super.key});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: Padding(
-          padding: EdgeInsetsGeometry.symmetric(horizontal: 16.w),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Center(
-                child: Image.asset(
-                  'assets/images/logo.png',
-                  width: 140.w,
-                  height: 140.h,
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).unfocus(); // Dismiss the keyboard
+      },
+      child: Scaffold(
+        body: SafeArea(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.w),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Center(
+                  child: Image.asset('assets/images/logo.png', width: 140.w),
                 ),
-              ),
-              Form(
-                child: CustomTextField(
-                  hintText: 'search',
-                  keyboardType: TextInputType.emailAddress,
-                  controller: controller.searchController,
-                  textInputAction: TextInputAction.next,
-                  inputStyle: AppTextStyles.body,
-                  hintStyle: AppTextStyles.hintText,
+                Form(
+                  child: CustomTextField(
+                    hintText: 'Search here',
+                    keyboardType: TextInputType.text,
+                    controller: controller.searchController,
+                    textInputAction: TextInputAction.search,
+                    inputStyle: AppTextStyles.body,
+                    hintStyle: AppTextStyles.hintText,
+                    suffixIcon: Icon(
+                      Icons.search,
+                      color: Colors.grey,
+                      size: 24.sp,
+                    ),
+                  ),
                 ),
-              ),
-              SizedBox(height: 15.h),
-              Expanded(
-                child: GridView.count(
-                  crossAxisCount: 2, 
-                  crossAxisSpacing: 10,
-                  mainAxisSpacing: 10,
-                  childAspectRatio: 0.55,
-                  children: List.generate(4, (index) {
-                    return Padding(
-                      padding: EdgeInsets.all(9),
-                      child: SizedBox(
-                        width: 160.w,
-                        height: 500.h,
-                        child: InkWell(
-                          onTap: (){
-                            print('tap $index');
-                          },
-                          child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Image.asset(
-                                'assets/images/jumbo-poster.png',
-                                width: 165.w,
-                                height: 200.h,
-                              ),
-                              Text(controller.movieTitle, style: AppTextStyles.label),
-                              Text(controller.movieSynopsis,
-                                style: AppTextStyles.smallText,
-                                // textAlign: TextAlign.justify,
-                                maxLines: 3,
-                                overflow: TextOverflow.ellipsis,
-                              ),
-                            ],
+                SizedBox(height: 16.h),
+                Expanded(
+                  child: GridView.count(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 4.w,
+                    mainAxisSpacing: 8.w,
+                    childAspectRatio: 0.55,
+                    children: List.generate(4, (index) {
+                      return Padding(
+                        padding: EdgeInsets.all(8.w),
+                        child: Material(
+                          color: Colors.transparent,
+                          borderRadius: BorderRadius.circular(12.r),
+                          clipBehavior: Clip.hardEdge,
+                          child: InkWell(
+                            onTap: () {
+                              print('tap $index');
+                            },
+                            child: CustomCard(controller: controller),
                           ),
                         ),
-                      ),
-                    );
-                  }),
+                      );
+                    }),
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
