349e5d2e578a91b707d469ea14f05e85b5ef4288
Mohamad Zaki Zakiran <zzakiran4@gmail.com>
2025-07-14 16:44:13 +0700

    added shared preferences

---
+++ lib/app/controller/storage_service_controller.dart
@@ -0,0 +1,13 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class StorageService {
+  Future<void> saveUsername(String username) async {
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setString('username', username);
+  }
+
+  Future<String?> getUsername() async {
+    final prefs = await SharedPreferences.getInstance();
+    return prefs.getString('username');
+  }
+}
