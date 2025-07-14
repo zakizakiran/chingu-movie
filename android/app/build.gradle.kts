cf31acbacd5af3395d554169375c98ec5687e226
Mohamad Zaki Zakiran <zzakiran4@gmail.com>
2025-07-14 16:43:53 +0700

    linked to firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,6 +38,11 @@ android {
             signingConfig = signingConfigs.getByName("debug")
         }
     }
+}
+
+dependencies {
+  implementation(platform("com.google.firebase:firebase-bom:33.16.0"))
+  implementation("com.google.firebase:firebase-analytics")
 }
 
 flutter {
