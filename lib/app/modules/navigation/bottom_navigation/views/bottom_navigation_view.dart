4d680a67b6024e6ac1470a1402bb38c37de1a652
Airin Ristiana <airinristianaaa@gmail.com>
2025-07-17 15:27:50 +0700

    navigasi update

--- lib/app/modules/navigation/bottom_navigation/views/bottom_navigation_view.dart
+++ lib/app/modules/navigation/bottom_navigation/views/bottom_navigation_view.dart
@@ -24,13 +24,13 @@ class BottomNavigationView extends GetView<BottomNavigationController> {
   Widget build(BuildContext context) {
     Get.lazyPut(() => HomeController());
     Get.lazyPut(() => OrderHistoryController());
-    Get.lazyPut(() => DashboardController());
+    Get.lazyPut(() => NotificationController());
     Get.lazyPut(() => ProfileController());
 
     final List<Widget> pages = const [
       HomeView(),
       OrderHistoryView(),
-      DashboardView(),
+      NotificationView(),
       ProfileView(),
     ];
 
