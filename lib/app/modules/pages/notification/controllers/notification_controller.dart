90be74ac41287d1f227f85e40634cdef6ebe5a47
Airin Ristiana <airinristianaaa@gmail.com>
2025-07-31 19:41:11 +0700

    notification page

--- lib/app/modules/pages/notification/controllers/notification_controller.dart
+++ lib/app/modules/pages/notification/controllers/notification_controller.dart
@@ -2,6 +2,19 @@ import 'package:get/get.dart';
 
 class NotificationController extends GetxController {
   //TODO: Implement NotificationController
+  final notifications =
+      [
+        {
+          'movieTitle': 'Judul Movie 1',
+          'showtimeMovie': '10.00 - 11.00 WIB',
+          'movieDate' : 'Thrusday, 26 Jun 2025'
+        },
+        {
+          'movieTitle': 'Judul Movie 1',
+          'showtimeMovie': '10.00 - 11.00 WIB',
+          'movieDate' : 'Thrusday, 26 Jun 2025'
+        },
+      ].obs;
 
   final count = 0.obs;
   @override
